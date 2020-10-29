67a954997c2a271138215b2168f617f6239ffc9d
olga2102 <70343745+olga2102@users.noreply.github.com>
2020-10-29 23:29:27 +0300

    Внесла правки

--- js/Form.js
+++ js/Form.js
@@ -1,7 +1,6 @@
 const popup = document.querySelector('.modal');
 const closePopupButton = document.querySelector('.button-close');
 
-closePopupButton.addEventListener('click', function (evt) { 
-    evt.preventDefault();
+closePopupButton.addEventListener('click', function () { 
     popup.classList.toggle('modal-close');
 });
